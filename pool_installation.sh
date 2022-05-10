96c0037fa7824a92fa1d4017c38678e7ee736985
oldtora <50335753+oldtora@users.noreply.github.com>
2022-05-11 01:18:43 +0300

    Update pool_installation.sh

--- pool_installation.sh
+++ pool_installation.sh
@@ -4,39 +4,34 @@ sudo apt-get install wget unzip -y
 
 ----------------------------------------
 
-#создаем папку kyve > заходим в неё > скачиваем зип файлы в папку (одной командой) (второй зип файл в будущем нужно будет изменить если планируете запускать другой пул)
+#создаем папку kyve > заходим в неё > скачиваем зип файл в папку (одной командой)
 
-mkdir $HOME/kyve; \ cd $HOME/kyve && \
-wget https://github.com/KYVENetwork/evm/releases/download/v1.0.3/evm-linux.zip && \
-wget https://github.com/kyve-org/near/releases/download/v0.0.0/kyve-near-linux.zip
+mkdir $HOME/kyve && \ cd $HOME/kyve && \ wget https://github.com/kyve-org/near/releases/download/v0.0.0/kyve-near-linux.zip
 
 ----------------------------------------
 
-# распаковываем файлы (одной командой)
+# распаковываем файлы
 
-unzip evm-linux.zip && \
 unzip kyve-near-linux.zip
 
 ----------------------------------------
 
 # даём максимальные пермиссии файлам (одной командой)
 
-chmod u+x * && \
-mv evm-linux /usr/bin/kyve-evm && \
-mv kyve-near-linux /usr/bin/kyve-near
+chmod u+x * && \ mv kyve-near-linux /usr/bin/kyve-near
 
 ----------------------------------------
 
 # создаём сервис и настраиваем валидатора: $POOL = номер пула который запускаете / "$MNEMONIC" = мнемоник кошелька с которым участвуете в тестнете / $STAKE = количество монет которые готовы отправить в стейкинг
 
-sudo tee <<EOF >/dev/null /etc/systemd/system/kyved.service
+sudo tee <<EOF >/dev/null /etc/systemd/system/near.service
 [Unit]
 Description=Kyve Node
 After=network.target
 [Service]
 Type=simple
 User=$USER
-ExecStart=$(which $BIN) \\
+ExecStart= /usr/bin/kyve-near \\
 --poolId $POOL \\
 --mnemonic "$MNEMONIC" \\
 --initialStake $STAKE \\
@@ -55,11 +50,11 @@ EOF
 # приводите в чувства все сервисы (одной командой)
 
 sudo systemctl daemon-reload && \
-sudo systemctl enable kyved && \
-sudo systemctl restart kyved
+sudo systemctl enable near && \
+sudo systemctl restart near
 
 ----------------------------------------
 
 # команда для проверки логов
 
-sudo journalctl -u kyved -f -o cat
+sudo journalctl -u near -f -o cat
