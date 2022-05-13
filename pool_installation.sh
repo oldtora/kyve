8196aca702341e1ea09d5aa69f6594e93caf0b22
oldtora <50335753+oldtora@users.noreply.github.com>
2022-05-14 01:30:38 +0300

    Update pool_installation.sh

--- pool_installation.sh
+++ pool_installation.sh
@@ -331,7 +331,7 @@ EOF
 
 # Запускаем сервис
 sudo systemctl daemon-reload && \ 
-sudo systemctl enable aevmos && \ 
+sudo systemctl enable evmos && \ 
 sudo systemctl restart evmos
 
 # Проверяем логи
